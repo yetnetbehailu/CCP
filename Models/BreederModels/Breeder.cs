5fc42648695ca8c1eb3ec4751e8ee1df15bff920
yeti <yetnet.behailu@hotmail.com>
2023-10-13 17:05:14 +0200

    Breeder Table Update

--- Models/BreederModels/Breeder.cs
+++ Models/BreederModels/Breeder.cs
@@ -9,8 +9,6 @@ namespace CCP.Models.BreederModels
         public string UserID { get; set; }
         public int? CountryID { get; set; }
         public string Name { get; set; }
-        public string Address { get; set; }
-        public string Mobile { get; set; }
 
         public CCPUser User { get; set; }
         public Country? Country { get; set; }
