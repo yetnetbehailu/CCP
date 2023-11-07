b054ec0e942cca32ffb4b56c8c5ea0117c79cd1f
yeti <yetnet.behailu@hotmail.com>
2023-11-07 09:41:59 +0100

    Enable user to login either using email or username

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -15,6 +15,8 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using CCP.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CCP.Areas.Identity.Pages.Account
 {
@@ -22,11 +24,14 @@ namespace CCP.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<CCPUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
+        private readonly CCPContext _context;
 
-        public LoginModel(SignInManager<CCPUser> signInManager, ILogger<LoginModel> logger)
+        public LoginModel(SignInManager<CCPUser> signInManager, ILogger<LoginModel> logger, CCPContext context)
         {
             _signInManager = signInManager;
             _logger = logger;
+            _context = context;
+
         }
 
         /// <summary>
@@ -66,8 +71,7 @@ namespace CCP.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+            public string EmailUsername { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -112,7 +116,12 @@ namespace CCP.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == Input.EmailUsername);
+                if(user == null)
+                {
+                    user = await _context.Users.FirstOrDefaultAsync(u => u.Email == Input.EmailUsername);
+                }
+                var result = await _signInManager.PasswordSignInAsync(user.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
